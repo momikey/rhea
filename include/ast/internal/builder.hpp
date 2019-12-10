bbb1b77d61d6443b6569c4c7e2f03c73869f9f03
Michael H. Potter <michael@potterpcs.net>
2019-12-10 15:07:45 -0500

    Sketching out an AST builder (not sure how I'm going to do this)

---
+++ include/ast/internal/builder.hpp
@@ -0,0 +1,12 @@
+#ifndef RHEA_AST_BUILDER_INTERNAL_HPP
+#define RHEA_AST_BUILDER_INTERNAL_HPP
+
+/*
+ * Internal detail for the AST builder.
+ */
+namespace rhea { namespace ast {
+    namespace internal {
+    }
+}}
+
+#endif /* RHEA_AST_BUILDER_INTERNAL_HPP */
